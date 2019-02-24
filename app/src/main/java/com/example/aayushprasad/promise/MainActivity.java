4935cf0ef8790c0758a7893f4809352d1554b24e
aayushprasad <reach.aayushp@gmail.com>
2019-02-23 19:03:30 -0500

    HTTP post complete

--- app/src/main/java/com/example/aayushprasad/promise/MainActivity.java
+++ app/src/main/java/com/example/aayushprasad/promise/MainActivity.java
@@ -10,6 +10,7 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,6 +21,18 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Locale;
 
@@ -36,6 +49,7 @@ public class MainActivity extends AppCompatActivity  implements LocationListener
     String provider;
     protected String latitude,longitude;
     protected boolean gps_enabled,network_enabled;
+    URL reqUrl;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,10 +64,25 @@ public class MainActivity extends AppCompatActivity  implements LocationListener
 
 
 
-        Button button = findViewById(R.id.Security);
-        button.setOnClickListener(new View.OnClickListener() {
+        Button search = findViewById(R.id.Search);
+        search.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                lat = -122.05;
+                lon = 36.7777;
                 System.out.println(lat+""+lon);
+                //the URL we will send the request to
+
+
+
+                JSONObject postData = new JSONObject();
+                try {
+                    postData.put("Lat: "+lat, "Hi");
+                    postData.put("Lon: "+lon, "hi");
+
+                    new SendDeviceDetails().execute("http://5a1fe5ef.ngrok.io/", postData.toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
 
             }
         });
@@ -64,10 +93,6 @@ public class MainActivity extends AppCompatActivity  implements LocationListener
     @Override
     public void onLocationChanged(Location location) {
         txtLat = (TextView) findViewById(R.id.textview1);
-        
-        lat = location.getLatitude();
-        lon = location.getLongitude();
-
 
     }
 
@@ -84,6 +109,53 @@ public class MainActivity extends AppCompatActivity  implements LocationListener
     @Override
     public void onProviderDisabled(String s) {
 
+    }
+
+    private class SendDeviceDetails extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String data = "";
+
+            HttpURLConnection httpURLConnection = null;
+            try {
+
+                httpURLConnection = (HttpURLConnection) new URL(params[0]).openConnection();
+                httpURLConnection.setRequestMethod("POST");
+
+                httpURLConnection.setDoOutput(true);
+
+                DataOutputStream wr = new DataOutputStream(httpURLConnection.getOutputStream());
+                wr.writeBytes("PostData=" + params[1]);
+                wr.flush();
+                wr.close();
+
+                InputStream in = httpURLConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(in);
+
+                int inputStreamData = inputStreamReader.read();
+                while (inputStreamData != -1) {
+                    char current = (char) inputStreamData;
+                    inputStreamData = inputStreamReader.read();
+                    data += current;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (httpURLConnection != null) {
+                    httpURLConnection.disconnect();
+                }
+            }
+
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            Log.e("TAG", result); // this is expecting a response code to be sent from your server upon receiving the POST data
+        }
     }
 
 
